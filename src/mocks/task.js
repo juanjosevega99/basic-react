d7e460362a442d7102ca9d3e940c7769bbae964c
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-02-10 23:34:44 -0500

    chore: add mock data

---
+++ src/mocks/task.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 1,
+    title: 'finish course react',
+    description: 'enjoy this weekend'
+  },
+  {
+    id: 2,
+    title: 'continue practicing and learning English',
+    description: 'enjoy this weekend'
+  },
+  {
+    id: 3,
+    title: 'Care about you',
+    description: 'enjoy this weekend'
+  },
+]
