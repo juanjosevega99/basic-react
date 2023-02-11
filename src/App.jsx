b4af07f020d66a5f6329cdc6165212b99a612369
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-02-10 23:18:48 -0500

    chore: create tasklist component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import TaskList from "./TaskList";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [countSad, setCountSad] = useState(0)
-
   return (
-    <div className="App">
-      <h1>Are you happy with your habits?</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Yeah, I'm on fire {count}
-        </button>
-        <button onClick={() => setCountSad((countSad) => countSad - 1)}>
-          No, I'm sad {countSad}
-        </button>
-      </div>
-    </div>
-  )
+    <>
+      <TaskList />
+    </>
+  );
 }
 
-export default App
+export default App;
