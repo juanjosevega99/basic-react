107ef0bf1d62396d0ecb90f0a4641c50daf2b6a1
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-02-11 09:31:21 -0500

    feat: show a message if there is not tasks right now

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -8,10 +8,14 @@ function TaskList() {
     setTasks(data);
   }, []);
 
+  if (tasks.length === 0) {
+    return <h1>No tasks right now</h1>
+  }
+
   return (
     <div>
       {tasks.map((task) => (
-        <div>
+        <div key={task.id}>
           <h3>{task.title}</h3>
           <p>{task.description}</p>
         </div>
