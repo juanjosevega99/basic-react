3a1be057dc7c4b4a9007706823c2678b7bdbfa25
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-02-12 22:21:07 -0500

    refactor: use context provider in main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,13 @@
-import { useState, useEffect } from "react";
-import { tasks as data } from "./mocks/task";
-
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
 
 import "./App.css";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    setTasks(data);
-  }, []);
-
-  function createTask({ title, description }) {
-    setTasks([
-      ...tasks,
-      {
-        id: tasks.length + 1,
-        title,
-        description,
-      },
-    ]);
-  }
-
-  function deleteTask(taskId) {
-    setTasks(tasks.filter((task) => task.id !== taskId));
-  }
-
   return (
     <>
-      <TaskForm createTask={createTask} />
-      <TaskList tasks={tasks} deleteTask={deleteTask} />
+      <TaskForm />
+      <TaskList />
     </>
   );
 }
