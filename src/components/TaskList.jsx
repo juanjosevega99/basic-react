bd912ebf563b208a9b2ac9cc992f3f02e8a10182
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-02-12 22:20:44 -0500

    refactor: using context instead props

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
 import TaskCard from "./TaskCard";
+import { TaskContext } from "../context/TaskContext";
 
-function TaskList({ tasks, deleteTask }) {
+function TaskList() {
+  const { tasks } = useContext(TaskContext);
+
   if (tasks.length === 0) {
     return <h1>No tasks right now</h1>;
   }
@@ -8,7 +12,7 @@ function TaskList({ tasks, deleteTask }) {
   return (
     <div>
       {tasks.map((task) => (
-        <TaskCard key={task.id} task={task} deleteTask={deleteTask} />
+        <TaskCard key={task.id} task={task} />
       ))}
     </div>
   );
